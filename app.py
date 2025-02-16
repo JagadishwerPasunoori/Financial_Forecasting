2a9225a29046709cc5acafb5fa726238b19b2054
JagadishwerPasunoori <Jagadishwer.Pasunoori99@gmail.com>
2025-02-17 01:16:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ def main():
     
     # Load models
     ml_model = load('models/ml_model.pkl')
-    dl_model = tf.keras.models.load_model('models/dl_model.h5')
+    dl_model = tf.keras.models.load_model(
+    'models/dl_model.keras',
+    custom_objects={'mse': tf.keras.losses.MeanSquaredError()}
+    )
     
     # Sidebar inputs
     with st.sidebar:
